ee4618f0ffe62630736fa0dd786379fd98dd5d30
Aleš Kučera <kuceral4@fel.cvut.cz>
2022-12-05 18:07:08 +0100

    Update

--- src/learning/utils.py
+++ src/learning/utils.py
@@ -11,7 +11,7 @@ def parse_data(data: tuple, device: torch.device):
     return image_batch, label_batch, idx_batch
 
 
-def create_model(cfg: DictConfig):
+def create_model2(cfg: DictConfig):
     model = _load_model(cfg.model.architecture, cfg.model.pretrained)
     n_inputs = cfg.train.n_inputs
     n_outputs = cfg.train.n_outputs
@@ -26,9 +26,16 @@ def create_model(cfg: DictConfig):
         model.classifier.high_classifier = Conv2d(128, n_outputs, kernel_size=(1, 1), stride=(1, 1))
     elif cfg.model.arch == 'fcn':
         model.classifier[-1] = Conv2d(512, n_outputs, kernel_size=(1, 1), stride=(1, 1))
-    elif cfg.model.arch == 'deeplabv3':
-        model.classifier[-1] = Conv2d(256, n_outputs, kernel_size=(1, 1), stride=(1, 1))
+    # elif cfg.model.arch == 'deeplabv3':
+    #     model.classifier[-1] = Conv2d(256, n_outputs, kernel_size=(1, 1), stride=(1, 1))
 
+    return model
+
+
+def create_model(cfg: DictConfig):
+    model = tms.deeplabv3_resnet101(num_classes=cfg.train.n_outputs)
+    model.backbone.conv1 = Conv2d(cfg.train.n_inputs, 64, kernel_size=(7, 7),
+                                  stride=(2, 2), padding=(3, 3), bias=False)
     return model
 
 
