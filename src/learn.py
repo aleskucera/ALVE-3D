98248958e4d6c63ef463d2007583ced5859734d6
Aleš Kučera <kuceral4@fel.cvut.cz>
2022-12-06 12:01:22 +0100

    Update

--- src/learn.py
+++ src/learn.py
@@ -12,18 +12,21 @@ from .learning import Trainer, create_model, Selector
 
 log = logging.getLogger(__name__)
 
+gpu_count = torch.cuda.device_count()
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 log.info(f'Using device {device}')
 
+
+# get gpu count
 
 def train_model(cfg: DictConfig):
     train_ds = SemanticDataset(cfg.ds.path, cfg.ds, split='train', size=cfg.train.dataset_size)
     val_ds = SemanticDataset(cfg.ds.path, cfg.ds, split='valid', size=cfg.train.dataset_size)
 
     train_loader = DataLoader(train_ds, batch_size=cfg.train.batch_size,
-                              shuffle=True, num_workers=os.cpu_count())
+                              shuffle=True, num_workers=gpu_count * 4)
     val_loader = DataLoader(val_ds, batch_size=cfg.train.batch_size,
-                            shuffle=True, num_workers=os.cpu_count())
+                            shuffle=True, num_workers=gpu_count * 4)
 
     model = create_model(cfg)
     model.to(device)
@@ -69,13 +72,13 @@ def train_model_active(cfg: DictConfig):
     unlabelled_ids = set(range(len(ds)))
 
     val_loader = DataLoader(val_ds, batch_size=cfg.train.batch_size,
-                            shuffle=True, num_workers=os.cpu_count())
+                            shuffle=True, num_workers=gpu_count * 4)
 
     unlabelled_ds = deepcopy(ds)
     # TODO: need to be tested, change n_iters=5 based on active learning termination criteria
     for _ in range(5):
         unlabelled_loader = DataLoader(unlabelled_ds, batch_size=cfg.train.batch_size,
-                                       shuffle=False, num_workers=os.cpu_count())
+                                       shuffle=False, num_workers=gpu_count * 4)
 
         log.info("Selecting data samples with high entropy...")
         train_ids = select_ids(loader=unlabelled_loader, model=model)
@@ -95,7 +98,7 @@ def train_model_active(cfg: DictConfig):
         log.info(f"Test dataset length: {len(unlabelled_ds)}")
 
         train_loader = DataLoader(train_ds, batch_size=cfg.train.batch_size,
-                                  shuffle=True, num_workers=os.cpu_count())
+                                  shuffle=True, num_workers=gpu_count * 4)
 
         trainer = Trainer(model=model, criterion=criterion, optimizer=optimizer,
                           metrics=metrics, device=device, log_path=cfg.path.output,
@@ -108,7 +111,7 @@ def test_model(cfg):
     test_ds = SemanticDataset(cfg.ds.path, cfg.ds, split='valid', size=cfg.test.dataset_size)
 
     test_loader = DataLoader(test_ds, batch_size=cfg.test.batch_size,
-                             shuffle=False, num_workers=os.cpu_count())
+                             shuffle=False, num_workers=gpu_count * 4)
 
     model_path = os.path.join(cfg.path.models, 'pretrained', cfg.test.model_name)
     model = torch.load(model_path).to(device)
