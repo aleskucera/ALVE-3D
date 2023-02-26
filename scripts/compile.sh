5f1700476987baee69da0487ff01f4119292ef12
Aleš Kučera <kuceral4@fel.cvut.cz>
2023-02-26 15:04:41 +0100

    Add ply_c

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-CONDA_ENV=/opt/conda/envs/ALVE-3D
-ALVE_3D=/home/kuceral4/ALVE-3D
-#ALVE_3D=/home/ales/Thesis/ALVE-3D
+#CONDA_ENV=/opt/conda/envs/ALVE-3D
+#ALVE_3D=/home/kuceral4/ALVE-3D
+
+CONDA_ENV=/home/ales/anaconda3/envs/ALVE-3D
+ALVE_3D=/home/ales/Thesis/ALVE-3D
 
 echo "---------- Compiling ply_c ----------"
 cd $ALVE_3D/src/superpoints/ply_c || echo "Didn't find ply_c"
