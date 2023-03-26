054678536e471149386a17a8a910c083d99e7ae9
Aleš Kučera <kuceral4@fel.cvut.cz>
2023-03-27 00:49:56 +0200

    Change dataset format

--- src/learn/main.py
+++ src/learn/main.py
@@ -73,8 +73,8 @@ def train_model(cfg: DictConfig, device: torch.device):
     #     val_ds = SemanticDataset(cfg.ds.path, cfg.ds, split='val', size=cfg.train.dataset_size, active_mode=True)
     #     selector = get_selector('entropy_voxels', train_ds.path, train_ds.get_dataset_clouds(), device)
     with wandb.init(project='Viewpoint Entropy Active Learning', group='Fully Labeled Training', name='Base'):
-        train_ds = SemanticDataset(cfg.ds.path, cfg.ds, split='train', size=cfg.train.dataset_size, active_mode=True)
-        val_ds = SemanticDataset(cfg.ds.path, cfg.ds, split='val', size=cfg.train.dataset_size, active_mode=True)
+        train_ds = SemanticDataset(cfg.ds.path, cfg.ds, split='train', size=cfg.train.dataset_size, active_mode=False)
+        val_ds = SemanticDataset(cfg.ds.path, cfg.ds, split='val', size=cfg.train.dataset_size, active_mode=False)
         selector = get_selector('entropy_voxels', train_ds.path, train_ds.get_dataset_clouds(), device)
 
         if load_voxels:
