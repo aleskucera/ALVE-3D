cd2358f60e95a779edd22e674b6e216817140de9
Aleš Kučera <kuceral4@fel.cvut.cz>
2023-04-01 22:44:50 +0200

    Change dataset format

--- src/learn/main.py
+++ src/learn/main.py
@@ -72,10 +72,10 @@ def train_model(cfg: DictConfig, device: torch.device):
     #     train_ds = SemanticDataset(cfg.ds.path, cfg.ds, split='train', size=cfg.train.dataset_size, active_mode=True)
     #     val_ds = SemanticDataset(cfg.ds.path, cfg.ds, split='val', size=cfg.train.dataset_size, active_mode=True)
     #     selector = get_selector('entropy_voxels', train_ds.path, train_ds.get_dataset_clouds(), device)
-        with wandb.init(project='Active Learning', group='Fully Labeled Training', name='Base'):
-            train_ds = SemanticDataset(cfg.ds.path, cfg.ds, split='train', size=cfg.train.dataset_size, active_mode=False)
-            val_ds = SemanticDataset(cfg.ds.path, cfg.ds, split='val', size=cfg.train.dataset_size, active_mode=False)
-            selector = get_selector('entropy_voxels', train_ds.path, train_ds.get_dataset_clouds(), device)
+    with wandb.init(project='Active Learning', group='Fully Labeled Training', name='Base'):
+        train_ds = SemanticDataset(cfg.ds.path, cfg.ds, split='train', size=cfg.train.dataset_size, active_mode=False)
+        val_ds = SemanticDataset(cfg.ds.path, cfg.ds, split='val', size=cfg.train.dataset_size, active_mode=False)
+        selector = get_selector('entropy_voxels', train_ds.path, train_ds.get_dataset_clouds(), device)
 
         if load_voxels:
             artifact = wandb.use_artifact(selected_voxels_artifact)
