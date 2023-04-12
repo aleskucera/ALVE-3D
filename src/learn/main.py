2e67a72bf66ba6144ac6e17db2e54c4a23f0a713
Aleš Kučera <kuceral4@fel.cvut.cz>
2023-04-12 21:33:25 +0200

    Major update

--- src/learn/main.py
+++ src/learn/main.py
@@ -71,7 +71,7 @@ def train_active(cfg: DictConfig, device: torch.device) -> None:
     # ========================================= Model Training =========================================
     # --------------------------------------------------------------------------------------------------
 
-    with wandb.init(project=f'AL - {project_name}', group='training', name=f'Training - {percentage}'):
+    with wandb.init(project=f'AL (test) - {project_name}', group='training', name=f'Training - {percentage}'):
 
         # Load Datasets
         train_ds = SemanticDataset(dataset_path=cfg.ds.path, project_name=project_name, cfg=cfg.ds,
@@ -125,7 +125,7 @@ def select_voxels(cfg: DictConfig, device: torch.device) -> None:
     select_percentage = cfg.active.select_percentage
     percentage = f'{cfg.active.expected_percentage_labeled + cfg.active.select_percentage}%'
 
-    with wandb.init(project=f'AL - {project_name}', group='selection', name=f'Selection - {percentage}'):
+    with wandb.init(project=f'AL (test) - {project_name}', group='selection', name=f'Selection - {percentage}'):
         dataset = SemanticDataset(dataset_path=cfg.ds.path, project_name=project_name,
                                   cfg=cfg.ds, split='train', size=cfg.train.dataset_size)
         selector = get_selector(selection_objects=selection_objects, criterion=criterion, dataset_path=cfg.ds.path,
@@ -177,7 +177,7 @@ def select_first_voxels(cfg: DictConfig, device: torch.device) -> None:
     select_percentage = cfg.active.select_percentage
     percentage = f'{cfg.active.expected_percentage_labeled + cfg.active.select_percentage}%'
 
-    with wandb.init(project=f'AL - {project_name}', group='selection', name=f'First Selection - {percentage}'):
+    with wandb.init(project=f'AL (test) - {project_name}', group='selection', name=f'First Selection - {percentage}'):
         dataset = SemanticDataset(dataset_path=cfg.ds.path, project_name=project_name,
                                   cfg=cfg.ds, split='train', size=cfg.train.dataset_size)
         selector = get_selector(selection_objects=selection_objects, criterion='random', dataset_path=dataset.path,
