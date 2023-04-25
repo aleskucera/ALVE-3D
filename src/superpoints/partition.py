5c72842ff1d1a84313cd3054f7b208806a900986
Aleš Kučera <kuceral4@fel.cvut.cz>
2023-04-25 15:52:42 +0200

    Resolve superpoint bug

--- src/superpoints/partition.py
+++ src/superpoints/partition.py
@@ -32,7 +32,7 @@ def partition_cloud(points: np.ndarray, edge_sources: np.ndarray, edge_targets: 
                                                  REG_STRENGTH,
                                                  cutoff=CP_CUTOFF, spatial=1,
                                                  weight_decay=0.2)
-    return np.array(components), np.array(component_map)
+    return np.array(components).astype(np.int64), np.array(component_map).astype(np.int64)
 
 
 def calculate_features(points: np.ndarray) -> dict:
