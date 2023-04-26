37fc2dc7c60502d73b22421de104d82aa70108d9
Aleš Kučera <kuceral4@fel.cvut.cz>
2023-04-26 16:49:10 +0200

    Create superpoints script

--- src/models/main.py
+++ src/models/main.py
@@ -8,8 +8,7 @@ from .salsanext import SalsaNext
 
 def get_model(cfg: DictConfig, device: torch.device):
     num_outputs = cfg.ds.num_classes
-    num_inputs = cfg.ds.num_semantic_channels
-    features = cfg.ds.num_partition_channels
+    num_inputs = cfg.ds.num_channels
 
     if cfg.model.architecture == 'SalsaNext':
         model = SalsaNext(num_inputs, num_outputs)
@@ -22,10 +21,10 @@ def get_model(cfg: DictConfig, device: torch.device):
             activation='softmax2d',
         )
 
-    elif cfg.model.architecture == 'PointNet':
-        model = PointNet(num_features=features['local'],
-                         num_global_features=features['global'],
-                         out_features=num_outputs)
+    # elif cfg.model.architecture == 'PointNet':
+    #     model = PointNet(num_features=features['local'],
+    #                      num_global_features=features['global'],
+    #                      out_features=num_outputs)
     else:
         raise ValueError(f'Unknown architecture: {cfg.model.architecture}')
 
